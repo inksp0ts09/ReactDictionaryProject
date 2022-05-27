5822ce6e52168975d693d2759d9439e679415249
Ann McGlone <92317108+inksp0ts09@users.noreply.github.com>
2022-05-27 10:31:19 -0400

    Added styling, Added calls to API for results.meaning, phonetic, synonyms, etc. added section styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./styles.css";
+import SheCodesLogo from "./SheCodesLogo.png";
+import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="container">
+        <header className="App-header">
+          <img src={SheCodesLogo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Maryanne McGlone</small>
+        </footer>
+      </div>
     </div>
   );
 }
